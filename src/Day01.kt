1419d954754147f215f96986282429d318372751
Thomas Bollmeier <developer@thomas-bollmeier.de>
2021-12-01 22:03:29 +0100

    Solved day 1 puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val depths = readInput("input01")
+        .map { it.toInt() }
+
+    //println(calcNumIncrements(depths))
+
+    val sums = createTriples(depths, depths.drop(1), depths.drop(2))
+        .map { it.first + it.second + it.third }
+
+    println(calcNumIncrements(sums))
+
+}
+
+fun calcNumIncrements(values: List<Int>): Int {
+    val steps = values zip values.drop(1)
+    return steps.filter { it.second > it.first }.size
+}
+
+tailrec fun <X, Y, Z> createTriples(
+    xs: List<X>,
+    ys: List<Y>,
+    zs: List<Z>,
+    acc: List<Triple<X, Y, Z>> = emptyList()
+): List<Triple<X, Y, Z>> {
+    return if (xs.isEmpty() || ys.isEmpty() || zs.isEmpty()) {
+        acc
+    } else {
+        val x = xs.first()
+        val y = ys.first()
+        val z = zs.first()
+        createTriples(
+            xs.drop(1),
+            ys.drop(1),
+            zs.drop(1),
+            acc + listOf(Triple(x, y, z))
+        )
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
 }
